f348d75302e783d4a737aa53a75a36f62208eb43
NesarAhmedRazon <grim.roy@gmail.com>
2021-10-13 02:07:09 +0600

    fix res_sec-1

--- lib/hero.php
+++ lib/hero.php
@@ -1,49 +1,46 @@
-<div class="container-fluid bg-white-3 hero">
+<div
+    class="bg-white-3 container-fluid font-lg-rem-6 font-md-rem-5 font-sm-rem-8 font-rem-5 font-xd-rem-7 font-xl-rem-6 font-xxd-rem-8 font-xxl-rem-6 hero">
     <div class="justify-content-center px-md-4 py-5 py-lg-3 py-md-0 py-xl-5 row">
         <div class="col-lg-11 col-12">
-            <div class="align-items-center my-0 my-md-5 row">
-                <div
-                    class="col-md-6 font-rem-4 font-xl-rem-8 offset-md-1 order-last order-md-first text-center text-md-start">
-                    <p class="font-em-7 font-md-em-5 mb-3 text-fh-5">Hi!</p>
-                    <h1 class="font-em-7 font-lg-em-9 font-xl-em-6 font-xxl-em-9 mb-3 text-bold text-lh-6">I’m Nesar
-                        Ahmed.<br>a
-                        Frontend
-                        developer.
+            <div class="align-items-center mry-em-0 mry-sm-em-2 mry-md-em-9 mry-xxd-em-16 row">
+                <div class="col-md-6 offset-md-1 order-last order-md-first text-center text-md-start">
+                    <p class="font-em-7 text-lit mrb-em-1">Hi!</p>
+                    <h1 class="font-em-6 mrb-em-2 text-bold">
+                        I’m Nesar Ahmed.<br>a Frontend developer.
                     </h1>
-                    <p class="font-em-4 font-xl-em-3 font-xxl-em-4 font-lg-em-4 font-md-em-3 mb-5 me-lg-5 text-lh-7">
+                    <p class="font-em-3 mrb-em-8 mrb-lg-em-12 mre-lg-em-18">
                         Frontend developer based in Chuadanga, Bangladesh. I am coding with a clean and beautiful
                         problem solving in mind.
                     </p>
-                    <div class="d-block d-md-table font-em-5 font-lg-em-3 font-lg-em-4 font-md-em-4 social w-100">
-                        <div class="d-md-table-row">
-                            <div
-                                class="align-middle d-md-table-cell mb-3 mb-md-0 pe-3 text-bold text-center text-md-start text-nowrap text-off">
-                                Follow me</div>
-                            <div
-                                class="font-em-6 font-xl-em-4 font-xxl-em-6 g-2 g-md-3 justify-content-center justify-content-lg-start justify-content-md-start me-lg-5 row row-cols-5 row-cols-lg-auto text-center text-md-start text-white">
-                                <div class="col"><a class="text-fh-6 icon-facebook" target="_blank"
-                                        href="https://bit.ly/3an5aRd"></a></div>
-                                <div class="col"><a class="text-fh-6 icon-upwork" target="_blank"
-                                        href="https://bit.ly/3DqbsvR"></a></div>
-                                <div class="col"><a class="text-fh-6 icon-twitter" target="_blank"
-                                        href="https://bit.ly/3loLd2T"></a></div>
-                                <div class="col"><a class="col text-fh-6 icon-linkedin" target="_blank"
-                                        href="https://bit.ly/3lqZbkT"></a></div>
-                                <div class="col"><a class="col text-fh-6 icon-github" target="_blank"
-                                        href="https://bit.ly/3lqZbkT"></a></div>
+                    <div class="container px-0 mrb-em-10 mrb-md-em-6">
+                        <div
+                            class="font-em-7 font-md-em-5 g-2 justify-content-center justify-content-md-start row row-cols-auto social text-white">
+                            <div class="col-12 text-off font-em-2 text-bold mrb-em-1 d-md-none">Follow Me</div>
+                            <div class="col">
+                                <a class="icon-facebook" target="_blank" href="https://bit.ly/3an5aRd"></a>
+                            </div>
+                            <div class="col">
+                                <a class="icon-upwork" target="_blank" href="https://bit.ly/3DqbsvR"></a>
+                            </div>
+                            <div class="col">
+                                <a class="icon-twitter" target="_blank" href="https://bit.ly/3loLd2T"></a>
+                            </div>
+                            <div class="col">
+                                <a class="icon-linkedin" target="_blank" href="https://bit.ly/3lqZbkT"></a>
+                            </div>
+                            <div class="col">
+                                <a class="icon-github" target="_blank" href="https://bit.ly/3lqZbkT"></a>
                             </div>
                         </div>
                     </div>
-
-
-                    <div class="row my-4 g-3">
-                        <div class="col-12 col-md-auto text-center">
-                            <a href="#" target="_self"
-                                class="bg-theme-3 btn font-md-em-3 px-4 round-em-2 shadow text-lh-10 w-100">Send
-                                Request</a>
+                    <div class="container px-0">
+                        <div class="row">
+                            <div class="col-12">
+                                <a href="#" target="_self"
+                                    class="bg-theme-3 btn font-em-3 font-md-em-2 px-4 round-em-2 shadow text-lh-10 wid-100 wid-md-auto">Send
+                                    Request</a>
+                            </div>
                         </div>
-
-
                     </div>
                 </div>
                 <div class="col-md-5 mb-5 mb-md-auto order-first order-md-last">
