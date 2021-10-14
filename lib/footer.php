cd5cae86a5c3a9fafeeb7991c669a9ab73ac064b
NesarAhmedRazon <grim.roy@gmail.com>
2021-10-14 22:22:49 +0600

    html validation

--- lib/footer.php
+++ lib/footer.php
@@ -31,15 +31,14 @@
         </div>
     </div>
 </div>
-</body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
     integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
-<script type="text/javascript" src="assets/slick/slick.min.js"></script>
-<script type="text/javascript" src="assets/app.js"></script>
+<script src="assets/slick/slick.min.js"></script>
+<script src="assets/app.js"></script>
 </body>
 
 </html>
